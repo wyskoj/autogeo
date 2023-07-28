415548644d9a0680a81d43a667d4f4ce9282c060
Jacob Wysko <jacob@wysko.org>
2023-07-27 20:24:45 -0400

    Refactor InverseAzimuth for clarity and edge case
    Changes in inverse-azimuth.ts include:
    - Enhanced comments to improve the understanding of the function
    - Refactored the implementation to increase readability
    - Added a condition to handle the case where dY equals 0, which helps to
     avoid division by zero errors.
    
    The updated comments and clearer implementation should make it easier
    for future developers to understand the logic. The edge case handling
    increases the robustness of the function.

--- operation/misc/inverse-azimuth.ts
+++ operation/misc/inverse-azimuth.ts
@@ -1,26 +1,32 @@
 import { StationCoordinates } from './station-coordinates';
 
 /**
- * An implementation of Ghilani's (11.5a) formula from Elementary Surveying.
- * @param point1 The coordinates of the first point.
- * @param point2 The coordinates of the second point.
+ * Calculates the azimuth from one point to another.
+ * It is an implementation of Ghilani's formula from Elementary Surveying
+ * (11.5a).
+ *
+ * @param point1 The StationCoordinates of the first point.
+ * @param point2 The StationCoordinates of the second point.
  * @returns The azimuth from point1 to point2.
  */
-export function InverseAzimuth(
-	point1: StationCoordinates,
-	point2: StationCoordinates
-): number {
+export function InverseAzimuth(point1: StationCoordinates, point2: StationCoordinates): number {
+
+	// Calculate the differences in the x and y coordinates of the two points
 	const dX = point2.x - point1.x;
 	const dY = point2.y - point1.y;
 
-	let C;
-	if (dX > 0 && dY > 0) {
-		C = 0;
-	} else if (dY < 0) {
-		C = Math.PI;
-	} else {
-		C = 2 * Math.PI;
+	// Handle the case when dY equals 0 to avoid division by zero
+	if (dY === 0) {
+		return dX > 0 ? Math.PI / 2 : 3 * Math.PI / 2;
 	}
 
+	// Initialize constant C based on the quadrant the angle lies in.
+	// If dX > 0 and dY > 0, C is 0, indicating the first quadrant.
+	// If dY < 0, regardless of dX, C is Math.PI, indicating the second or third quadrant.
+	// Otherwise, C is 2 * Math.PI, indicating the fourth quadrant.
+	const C = dX > 0 && dY > 0 ? 0 : dY < 0 ? Math.PI : 2 * Math.PI;
+
+	// Calculate the inverse azimuth, adjusting for quadrant by adding C to atan(dX / dY)
+	// Ensure the final result is between 0 and 2 * Math.PI by taking the result modulo 2 * Math.PI
 	return (C + Math.atan(dX / dY)) % (2 * Math.PI);
 }

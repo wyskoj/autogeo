82b29c3e830086457772a0baf484fbae398be3b1
Jacob Wysko <jacob@wysko.org>
2023-07-27 20:28:36 -0400

    Refactor angle-angle intersection, comments
    Enhanced the clarity and readability of code in
    angle-angle-intersection-comp.ts. The code does the calculations
    necessary to find the intersection point between two angles. Improved
    comments were added to provide a clearer understanding of what the
    calculations do and why they are necessary. Fully explained the
    trigonometry used in code including InverseAzimuth calculations and
    sine/cosine applications. In addition, a check was added to handle cases
    where the intersection is not possible. This refactoring aims to make
    code more understandable for future maintenance.

--- operation/coordinate-geometry/angle-angle-intersection/angle-angle-intersection-comp.ts
+++ operation/coordinate-geometry/angle-angle-intersection/angle-angle-intersection-comp.ts
@@ -5,33 +5,41 @@ import { InverseAzimuth } from '../../misc/inverse-azimuth';
 export function AngleAngleIntersectionComp(
 	data: AngleAngleIntersectionData
 ): AngleAngleIntersectionResult {
-	// This is basically a Bearing-Bearing intersection, just with an extra step.
-	const AzOB1 = InverseAzimuth(data.occupiedStation1, data.backsightStation1);
-	const AzOI1 = AzOB1 + data.angleFromStation1;
 
-	const AzOB2 = InverseAzimuth(data.occupiedStation2, data.backsightStation2);
-	const AzOI2 = AzOB2 + data.angleFromStation2;
+	// Compute the azimuths for the first station
+	const AzOB1 = InverseAzimuth(data.occupiedStation1, data.backsightStation1); // Compute the azimuth between the occupied station and the backsight station
+	const AzOI1 = AzOB1 + data.angleFromStation1; // Determine the azimuth of the intersection from the first station
 
+	// Compute the azimuths for the second station
+	const AzOB2 = InverseAzimuth(data.occupiedStation2, data.backsightStation2); // Compute the azimuth between the occupied station and the backsight station
+	const AzOI2 = AzOB2 + data.angleFromStation2; // Determine the azimuth of the intersection from the second station
+
+	// Compute the distance between the two occupied stations
 	const AB = Math.sqrt(
 		(data.occupiedStation1.x - data.occupiedStation2.x) ** 2 +
-			(data.occupiedStation1.y - data.occupiedStation2.y) ** 2
+		(data.occupiedStation1.y - data.occupiedStation2.y) ** 2
 	);
 
+	// Compute the azimuth between the two occupied stations
 	const AzAB = InverseAzimuth(data.occupiedStation1, data.occupiedStation2);
 
-	const A = AzOI1 - AzAB;
-	const B = Math.PI + AzAB - AzOI2;
-	const P = Math.PI - A - B;
+	// Calculate the angles for determining the intersection points
+	const A = AzOI1 - AzAB; // Angle to the intersection from the first station
+	const B = Math.PI + AzAB - AzOI2; // Angle to the intersection from the second station
+	const P = Math.PI - A - B; // The known internal angle of the triangle
 
-	const AP = AB * (Math.sin(B) / Math.sin(P));
+	const AP = AB * (Math.sin(B) / Math.sin(P)); // Length from first station to intersection point
 
+	// Calculate the intersection point
 	const x = data.occupiedStation1.x + AP * Math.sin(AzOI1);
 	const y = data.occupiedStation1.y + AP * Math.cos(AzOI1);
 
+	// Check if intersection point is real
 	if (isNaN(x) || isNaN(y) || !isFinite(x) || !isFinite(y)) {
 		throw Error('There is no solution for the given data.');
 	}
 
+	// Return the intersection point as a solution
 	return {
 		solution: {
 			x,
